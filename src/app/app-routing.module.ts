d7d8ccea0c996631c1b574148690e1168f17df15
Celso Cedillo Flores <ccedillo@emapag-ep.gob.ec>
2019-12-17 12:47:19 -0500

    revisa los datos del api con node, y antes de implementar datatables para desistir de aggrid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ActasComponent } from './pages/acta/actas.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'actas', component: ActasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+//import { AgGridModule } from '@ag-grid-community/angular';
+import { DataTablesModule} from 'angular-datatables';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/shared/header/header.component';
+import { ActasComponent } from './pages/acta/actas.component';
+
+import { ActasService } from './services/actas.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ActasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    DataTablesModule
+    //AgGridModule.withComponents([])
   ],
-  providers: [],
+  providers: [ActasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
