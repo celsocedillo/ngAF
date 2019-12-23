ecfdffe030164e1a500541b1aaf172f225d9fdc0
Celso Cedillo Flores <ccedillo@emapag-ep.gob.ec>
2019-12-22 21:40:03 -0500

    Con github desktop
    Para registrar los cambios que no se registran con la linea de comandos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 //import { AgGridModule } from '@ag-grid-community/angular';
-import { DataTablesModule} from 'angular-datatables';
-
+//import { DataTablesModule} from 'angular-datatables';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +10,12 @@ import { HeaderComponent } from './pages/shared/header/header.component';
 import { ActasComponent } from './pages/acta/actas.component';
 
 import { ActasService } from './services/actas.service';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ListboxModule } from 'primeng/listbox';
 
 @NgModule({
   declarations: [
@@ -22,7 +27,13 @@ import { ActasService } from './services/actas.service';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    DataTablesModule
+    TableModule,
+    DropdownModule,
+    MultiSelectModule,
+    BrowserAnimationsModule,
+    OverlayPanelModule,
+    ListboxModule
+    //DataTablesModule
     //AgGridModule.withComponents([])
   ],
   providers: [ActasService],
